4e4fa781104dbcf39a0c7692e91fa7f7244e1dbd
Dockerizer for PHP <email@example.com>
2022-04-08 12:34:46 +0300

    4 layout and blocks: added Yaroslav/luma theme and demo layout files

---
+++ app/design/frontend/Yaroslav/luma/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Yaroslav/luma', __DIR__);
