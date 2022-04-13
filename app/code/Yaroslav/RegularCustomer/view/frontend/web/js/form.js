3c48088baea4d8637b8c69317acbde582e9bd523
Dockerizer for PHP <email@example.com>
2022-04-13 17:24:51 +0300

    11 - JavaScript in Magento 2. CSP: Using Magento alert widget instead of the native browser alert

--- app/code/Yaroslav/RegularCustomer/view/frontend/web/js/form.js
+++ app/code/Yaroslav/RegularCustomer/view/frontend/web/js/form.js
@@ -1,11 +1,17 @@
 define([
-    'jquery'
+    'jquery',
+    'Magento_Ui/js/modal/alert'
     // Other dependencies go here
 ], function ($) {
     'use strict';
 
     return function (message) {
         // Do the stuff here
-        alert(message);
+        $('<div>').text($.mage.__('Foo Bar Baz')).alert({
+            title: $.mage.__(message),
+            actions: {
+                always: function(){}
+            }
+        });
     };
 });
