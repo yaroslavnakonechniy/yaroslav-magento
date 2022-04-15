71c93af41e82b6c0ab374d3d309ce09803dcb34d
Dockerizer for PHP <email@example.com>
2022-04-15 12:21:08 +0300

    13 - Session: Implemented Redis cache and session storage

--- app/etc/old.env.php
+++ app/etc/old.env.php
@@ -36,48 +36,17 @@ return [
         ]
     ],
     'x-frame-options' => 'SAMEORIGIN',
-    'MAGE_MODE' => 'production',
+    'MAGE_MODE' => 'developer',
     'session' => [
-        'save' => 'redis',
-        'redis' => [
-            'host' => '127.0.0.1',
-            'port' => '6379',
-            'password' => '',
-            'timeout' => '2.5',
-            'persistent_identifier' => '',
-            'database' => '5',
-            'compression_threshold' => '2048',
-            'compression_library' => 'gzip',
-            'log_level' => '4',
-            'max_concurrency' => '6',
-            'break_after_frontend' => '5',
-            'break_after_adminhtml' => '30',
-            'first_lifetime' => '600',
-            'bot_first_lifetime' => '60',
-            'bot_lifetime' => '7200',
-            'disable_locking' => '0',
-            'min_lifetime' => '60',
-            'max_lifetime' => '2592000'
-        ]
+        'save' => 'files'
     ],
     'cache' => [
         'frontend' => [
             'default' => [
-                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
-                'backend_options' => [
-                    'server' => '127.0.0.1',
-                    'database' => '0',
-                    'port' => '6379'
-                ]
+                'id_prefix' => '69d_'
             ],
             'page_cache' => [
-                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
-                'backend_options' => [
-                    'server' => '127.0.0.1',
-                    'port' => '6379',
-                    'database' => '1',
-                    'compress_data' => '0'
-                ]
+                'id_prefix' => '69d_'
             ]
         ],
         'allow_parallel_generation' => false
