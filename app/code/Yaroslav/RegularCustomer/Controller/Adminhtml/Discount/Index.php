cd13aaf383b39dd8a54c7fcc9ac6beaddf5b2acc
Dockerizer for PHP <email@example.com>
2022-04-26 08:39:33 +0300

    18 - Admin Panel listings: Added a new page for listing in Admin Panel

---
+++ app/code/Yaroslav/RegularCustomer/Controller/Adminhtml/Discount/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yaroslav\RegularCustomer\Controller\Adminhtml\Discount;
+
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Index extends \Magento\Backend\App\Action implements \Magento\Framework\App\Action\HttpGetActionInterface
+{
+    public const ADMIN_RESOURCE = 'Yaroslav_RegularCustomer::listing';
+
+    /**
+     * @inheritDoc
+     */
+    public function execute(): ResultInterface
+    {
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->getConfig()->getTitle()->prepend(__('Discount Requests'));
+
+        return $resultPage;
+    }
+}
